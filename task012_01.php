75aa0ba79ad3f7d048f3b3016a14ff491eb1471e
olegmxjack <olegmxjack@rambler.ru>
2016-03-03 10:04:01 +0200

    task 12

---
+++ task012_01.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<form action="task012_02.php" method="POST">
+<input type="text" name="city">
+<input type="submit" value="GO">
+	
+</form>
+</body>
+</html>
---
+++ task012_02.php
@@ -0,0 +1,2 @@
+<?php 
+echo 'Your city is '.$_POST['city'];
