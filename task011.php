737481dcea5bc3dccd42e38d54bdffde7890033d
olegmxjack <olegmxjack@rambler.ru>
2016-03-03 09:11:25 +0200

    task 10

---
+++ task011.php
@@ -0,0 +1,19 @@
+<?PHP 
+
+$x = 10;
+$y = 7;
+
+$res = $x + $y;
+print "$x + $y = $res<br>";
+
+$res = $x - $y;
+print "$x - $y = $res<br>";
+
+$res = $x * $y;
+print "$x * $y = $res<br>";
+
+$res = $x / $y;
+print "$x / $y = $res<br>";
+
+$res = $x % $y;
+print "$x % $y = $res<br>";
